4cd5ad1937bc3f7cfd5a159a11519311db0b377a
asimpat <asimoko@yahoo.com>
2022-10-14 13:23:41 +0100

    modified strategy

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,7 +3,7 @@ import { AuthService } from './auth.service';
 import { PassportModule } from '@nestjs/passport';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constant';
-import { JwtStrategy } from './jwtStrategy';
+import { JwtStrategy } from './strategy/jwtStrategy';
 import { UserModule } from 'src/user/user.module';
 import { UserService } from 'src/user/user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
